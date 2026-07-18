d33d9fea946b8b5dd55d906dc9139ab7a1bae9af
Soumyasree Dora <dora.soumya144@gmail.com>
2026-07-19 00:13:51 +0530

    Setup React frontend with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>🚔 CrimeLens AI</h1>
+
+      <h2>AI Powered Crime Analytics Dashboard</h2>
+
+      <p>
+        Analyze crime trends, visualize hotspots, and predict future crime
+        patterns using Artificial Intelligence.
+      </p>
+
+      <button>Explore Dashboard</button>
+    </div>
+  );
+}
+
+export default App;
